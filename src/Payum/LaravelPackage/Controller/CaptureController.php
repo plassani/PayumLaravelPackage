88f908236166cca57c03020cf1b84f6b2e65b599
Nicolas Plassart <7387553+plassani@users.noreply.github.com>
2023-01-26 15:30:29 +0100

    Update CaptureController.php
    Lumen compatibility

--- src/Payum/LaravelPackage/Controller/CaptureController.php
+++ src/Payum/LaravelPackage/Controller/CaptureController.php
@@ -7,10 +7,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class CaptureController extends PayumController
 {
-    public function doAction($payum_token)
+    public function doAction($request, $payum_token)
     {
-        /** @var Request $request */
-        $request = \App::make('request');
         $request->attributes->set('payum_token', $payum_token);
 
         $token = $this->getPayum()->getHttpRequestVerifier()->verify($request);
@@ -21,7 +19,7 @@ class CaptureController extends PayumController
         } catch (ReplyInterface $reply) {
             return $this->convertReply($reply);
         }
-        
+
         $this->getPayum()->getHttpRequestVerifier()->invalidate($token);
 
         return \Redirect::to($token->getAfterUrl());
